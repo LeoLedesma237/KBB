8223233b2cc5ad2a78fc8b55555099a2c2de1baf
LeoLedesma237 <leandroledesma237@yahoo.com>
2024-10-15 19:57:49 -0500

    BRIEF2 Updates

--- Scoring/BRIEF2ParentForm.Scoring.R
+++ Scoring/BRIEF2ParentForm.Scoring.R
@@ -1,4 +1,4 @@
-# This is the script for scoring BRIEF2 Self Report
+# This is the script for scoring BRIEF2 Parent Form
 
 # Load in Packages
 library(tidyverse)
@@ -6,22 +6,22 @@ library(readxl)
 library(openxlsx)
 
 # Read in the file
-BRIEF2SR <- read_excel(paste0(DataLocation,"RAW_DATA/Behavioral/Children/BRIEF2_SF_Raw.xlsx"))
+BRIEF2PF <- read_excel(paste0(DataLocation,"RAW_DATA/Behavioral/Adults/BRIEF2_Parent_Raw.xlsx"))
 
 # Check the IDs for errors
 source("Scoring/IDError_FUNCTION.R")
-BRSR_Notes <-check_id_errors("BRIEF2 Self Report",
-                           BRIEF2SR$Child_ID)
+BRPF_Notes <-check_id_errors("BRIEF2 Self Report",
+                             BRIEF2PF$Child_ID)
 
 
 # Select Vars of Interest
-Front <- BRIEF2SR %>%
+Front <- BRIEF2PF %>%
   select(Child_ID,
          Evaluator_ID,
          Date_of_Evaluation)
 
-Items <- BRIEF2SR %>%
-  select(`_1`:`_55`)
+Items <- BRIEF2PF %>%
+  select(`_1`:`_63`)
 
 # Rename Items (Part 1)
 names(Items) <- 1:length(Items)
@@ -36,19 +36,18 @@ Items2 <- Items %>%
   ))
 
 # Cbind them
-BRIEF2SR2 <- tibble(cbind(Front,Items2))
+BRIEF2PF2 <- tibble(cbind(Front,Items2))
 
 # Count the number of NAs, items given, and total number of items
-BRIEF2SR2$NA.Num <- rowSums(is.na(Items2))
-BRIEF2SR2$Items.Given <- rowSums(!(is.na(Items2)))
-BRIEF2SR2$Total.Items <- length(Items2)
+BRIEF2PF2$NA.Num <- rowSums(is.na(Items2))
+BRIEF2PF2$Items.Given <- rowSums(!(is.na(Items2)))
+BRIEF2PF2$Total.Items <- length(Items2)
 
 # Change The names of the variables so we know it is pattern reasoning
-names(BRIEF2SR2) <- c(names(BRIEF2SR2)[1:3], paste("BRSR_",names(BRIEF2SR2)[4:61], sep=""))
-
+names(BRIEF2PF2) <- c(names(BRIEF2PF2)[1:3], paste("BRPF_",names(BRIEF2PF2)[4:69], sep=""))
 
 # Load in the subtest information
-BRIEF2_subtests <- read_excel(paste0(DataLocation,"RAW_DATA/AnswerKeys/BRIEF2SelfReport.xlsx"))
+BRIEF2_subtests <- read_excel(paste0(DataLocation,"RAW_DATA/AnswerKeys/BRIEF2ParentForm.xlsx"))
 
 # Get the subtest as a variable
 subtest <- BRIEF2_subtests$Subtest
@@ -65,10 +64,10 @@ RowSums_list <- list()
 for(ii in 1:length(allConstructs)) {
   
   # Extract the column names for each construct 
-   currentConstruct <- Items2[,grepl(pattern = allConstructs[ii], subtest)]
+  currentConstruct <- Items2[,grepl(pattern = allConstructs[ii], subtest)]
   
   # Calculate the Row means
-   ConstructScored <- data.frame(rowSums(currentConstruct))
+  ConstructScored <- data.frame(rowSums(currentConstruct))
   
   # Rename the variable
   names(ConstructScored) <- allConstructs[ii]
@@ -86,17 +85,17 @@ AllConstructsScored <- select(AllConstructsScored, -`F`)
 # Introduce a couple more variables 
 AllConstructsScored <- AllConstructsScored %>%
   mutate(BRI = Inhibit + Self_Monitor,
-         ERI = Emotional_Control + Shift,
-         CRI = Task_Completion + Working_Memory + Plan_Organize,
+         ERI = Shift + Emotional_Control ,
+         CRI = Initiate + Working_Memory + Plan_Organize + Task_Monitor + Organization_of_Materials,
          GEC = BRI + ERI + CRI)
 
 
 # Now introduce all of these scored measures into the final dataset
-BRIEF2SR3 <- cbind(BRIEF2SR2, AllConstructsScored)
+BRIEF2PF3 <- cbind(BRIEF2PF2, AllConstructsScored)
 
 
 # Now let's score the validity portion
-BREIF2_validity <- read_excel(paste0(DataLocation,"RAW_DATA/AnswerKeys/BRIEF2SelfReport.xlsx"), sheet = "Validity")
+BREIF2_validity <- read_excel(paste0(DataLocation,"RAW_DATA/AnswerKeys/BRIEF2ParentForm.xlsx"), sheet = "Validity")
 
 # Save the validity questions as an object
 Validity <- paste0("Q",BREIF2_validity$Question,"_",BREIF2_validity$Validity)
@@ -111,48 +110,48 @@ Inconsistency1 <- Items2[,grepl(pattern = "Inconsistency1", Validity)]
 Inconsistency2 <- Items2[,grepl(pattern = "Inconsistency2", Validity)]
 
 # Scoring Negativity
-BRIEF2SR3$NegativityScore <- rowSums(Negativity == 3)
-BRIEF2SR3 <- mutate(BRIEF2SR3, Negativity = case_when(
-                               NegativityScore <=3 ~ "Acceptable",
-                               NegativityScore == 4 ~ "Elevated",
-                               NegativityScore >= 5 ~ "Highly_Elevated"
+BRIEF2PF3$NegativityScore <- rowSums(Negativity == 3)
+BRIEF2PF3 <- mutate(BRIEF2PF3, Negativity = case_when(
+  NegativityScore <=3 ~ "Acceptable",
+  NegativityScore == 4 ~ "Elevated",
+  NegativityScore >= 5 ~ "Highly_Elevated"
 ))
 
 # Scoring Infrequency
-BRIEF2SR3$InfrequencyScore <- rowSums(Infrequency >= 2)
-BRIEF2SR3 <- mutate(BRIEF2SR3, Infrequency = case_when(
-                               InfrequencyScore == 0 ~ "Acceptable",
-                               InfrequencyScore >= 1 ~ "Questionable",
+BRIEF2PF3$InfrequencyScore <- rowSums(Infrequency >= 2)
+BRIEF2PF3 <- mutate(BRIEF2PF3, Infrequency = case_when(
+  InfrequencyScore == 0 ~ "Acceptable",
+  InfrequencyScore >= 1 ~ "Questionable",
 ))
 
 # Scoring Inconsistency (Tedious)
 # Change the order of inconsistency 2 so the matrices can match
-Inconsistency2 <- select(Inconsistency2, Q12_Inconsistency2:Q27_Inconsistency2,
-                         Q52_Inconsistency2,Q41_Inconsistency2, Q42_Inconsistency2,
-                         Q55_Inconsistency2, Q53_Inconsistency2)
-
-BRIEF2SR3$InconsistencyScore <- abs(rowSums(Inconsistency1 - Inconsistency2))
-BRIEF2SR3 <- mutate(BRIEF2SR3, Inconsistency = case_when(
+Inconsistency2 <- Inconsistency2 %>%
+  select(Q21_Inconsistency2, Q55_Inconsistency2, Q48_Inconsistency2, Q40_Inconsistency2,
+         Q26_Inconsistency2, Q56_Inconsistency2, Q50_Inconsistency2, Q63_Inconsistency2)
+    
+    
+BRIEF2PF3$InconsistencyScore <- abs(rowSums(Inconsistency1 - Inconsistency2))
+BRIEF2PF3 <- mutate(BRIEF2PF3, Inconsistency = case_when(
   InconsistencyScore <= 5 ~ "Acceptable",
   InconsistencyScore == 6 | InconsistencyScore == 7 ~ "Questionable",
   InconsistencyScore >= 8 ~ "Inconsistent"
 ))
 
 
-
 # Create a save pathway
-save.pathway_BRSR <- paste(DataLocation,"FINAL_DS/Behavioral/Children/",
-                         "BRIEF2_SF.xlsx", sep="")
+save.pathway_BRPF <- paste(DataLocation,"FINAL_DS/Behavioral/Adults/",
+                           "BRIEF2_Parent.xlsx", sep="")
 
 # Save the scored data
-write.xlsx(x= BRIEF2SR3, file = save.pathway_BRSR)
+write.xlsx(x= BRIEF2PF3, file = save.pathway_BRPF)
 
 # Create a save pathway for Notes
 save.pathway.notes <- paste(DataLocation,"REPORTS/Individual/",
-                            "BRIEF2_SF.csv", sep="")
+                            "BRIEF2_Parent.csv", sep="")
 
 # Save the Notes as a CSV
-write_csv(x = BRSR_Notes, save.pathway.notes)
+write_csv(x = BRPF_Notes, save.pathway.notes)
 
 
 # Make a note that the data was saved successfully
@@ -163,4 +162,3 @@ rm(list = ls()[!(ls() %in% c("DataLocation"))])
 
 # Set a pause time for 1 second
 Sys.sleep(1)
-
